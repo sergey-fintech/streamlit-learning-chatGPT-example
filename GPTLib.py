21ca0f17500c6d6ee7b95f355fb81898f49037af
sergey-fintech <113521045+sergey-fintech@users.noreply.github.com>
2023-06-02 14:01:10 +0300

    init
    init

---
+++ GPTLib.py
@@ -0,0 +1,86 @@
+from langchain.llms import OpenAI
+from langchain.docstore.document import Document
+import requests
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.prompts import PromptTemplate
+import pathlib
+import subprocess
+import tempfile
+import ipywidgets as widgets
+import os
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import re
+import os
+import openai
+import tiktoken
+import re
+
+
+class GPT():
+  def __init__(self):
+    pass
+
+
+  def load_search_indexes(self, url: str) -> str:
+    # Извлекаем из URL идентификатор документа для доступа по прямой ссылке
+    match = re.search('/document/d/([a-zA-Z0-9-_]+)', url)
+    if match is None:
+        raise ValueError('Некорректная ссылка Google Docs')
+    docID = match.group(1)
+
+    # Скачиваем документ как текстовый файл
+    response = requests.get(f'https://docs.google.com/document/d/{docID}/export?format=txt')
+    response.raise_for_status()
+    text = response.text
+    # Проведём векторизация документа
+    return self.create_embedding(text)
+  
+  def update_token(self, value):
+    openai.api_key = value
+    os.environ["OPENAI_API_KEY"] = openai.api_key
+    print(f'Ключ сохранен!')
+
+  
+  def create_embedding(self, data):
+    
+    # Получение числа токенов в строке
+    def num_tokens_from_string(string: str, encoding_name: str) -> int:
+      encoding = tiktoken.get_encoding(encoding_name)
+      num_tokens = len(encoding.encode(string))
+      return num_tokens
+
+    #  Разбивает текст на куски
+    source_chunks = []
+    splitter = CharacterTextSplitter(separator="\n", chunk_size=1024, chunk_overlap=0)
+
+    for chunk in splitter.split_text(data):
+      source_chunks.append(Document(page_content=chunk, metadata={}))
+
+    # Индексируем документ по частям и заносим в векторную базу данных
+    search_index = Chroma.from_documents(source_chunks, OpenAIEmbeddings(), )
+    # Посчитаем токены
+    count_token = num_tokens_from_string(' '.join([x.page_content for x in source_chunks]), "cl100k_base")
+    billing = 0.0004*(count_token/1000) 
+    return search_index, count_token, billing
+
+    
+  def answer_index(self, system, topic, search_index, temp = 1): 
+    # Поиск документов по схожести с вопросом 
+    docs = search_index.similarity_search(topic, k=5)
+    message_content = re.sub(r'\n{2}', ' ', '\n '.join([f'\nОтрывок документа №{i+1}\n=====================' + doc.page_content + '\\n' for i, doc in enumerate(docs)]))
+   
+    messages = [
+      {"role": "system", "content": system + f"{message_content}"},
+      {"role": "user", "content": topic}
+      ]
+
+    completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=messages,
+    temperature=temp
+    )
+
+    return completion.choices[0].message.content, 0.002*(completion["usage"]["total_tokens"]/1000)
--- streamlit-chatGPT-app.py
+++ streamlit-chatGPT-app.py
@@ -1,5 +1,89 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+from GPTLib import GPT
 
-st.title('Дообучение ChatGPT на документе Google Doc')
+gpt = GPT()
+
+def preTrain(token, doc):
+  try:
+    if token is None:
+      st.write('Для обучения нужен ключ от ChatGPT')
+    else:
+      gpt.update_token(token)
+      ind, count_token, billing = gpt.load_search_indexes(doc)
+
+      st.write(f"Количество токенов в документе: {count_token}") 
+      st.write(f"СТОИМОСТЬ ОБУЧЕНИЯ МОДЕЛИ: {billing}$") 
+      return ind
+
+  except Exception as e:
+    st.write(f"ОШИБКА: {e}")
+    return None
+
+def answer(prompt, query, indexes):
+  if indexes is None:
+    st.write('Модель необходимо обучить!')
+  else:
+    try:
+      content, billing= gpt.answer_index(
+        prompt,
+        query,
+        indexes
+      )
+      st.write(f"ОТВЕТ: {content}") 
+      st.write(f"СТОИМОСТЬ ЗАПРОСА: {billing}$") 
+    except Exception as e:
+      st.write(f"ОШИБКА: {e}")
+
+
+def main(): 
+
+  if "learn" not in st.session_state:
+    st.session_state.learn = None
+
+
+  def answer_request():
+    if len(st.session_state['prompt'])>16 and len(st.session_state['query'])>10:      
+      answer(st.session_state['prompt'], st.session_state['query'], st.session_state.learn)
+
+    
+  
+  def input_calback():
+    if len(st.session_state['token'])>16 and len(st.session_state['doc'])>16:
+      st.session_state.learn = preTrain(st.session_state['token'], st.session_state['doc'])
+
+  st.header("Дообучение ChatGPT на документе Google Doc")
+  st.subheader("Откройте левую боковую панель и задайте параметры")
+
+  st.sidebar.text_input('Ключ доступа', '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', key='token', on_change=input_calback)
+  st.sidebar.text_input('Данные для обучения (ссылка на гугл документ)', 'https://docs.google.com/document/d/1MuQ02a3Kz6ysDN43SM5YrURS2Mg2trLNrM_TyIElZLQ/edit?usp=sharing', key='doc', on_change=input_calback)
+  
+  st.sidebar.text_area('Инструкции для модели', 
+      """Инструкция по оценке диалога менеджера по продажам с клиентом
+
+       Перед тобой диалог менеджера по продажам с клиентом
+       Тебе надо проверить несколько критериев и заполнить отчёт
+       Не пиши общее сообщение, только отчёт по форме, форма отчёта будет в конце
+
+       Что надо проверить
+       1. Говорил ли клиент о потребности в трудоустройстве
+       2. Говорил ли клиент о потребности создать AI проект
+       3. Говорил ли клиент возражение о стоимости обучения
+       4. Говорил ли клиент возражение о времени на обучение
+
+       Форма отчёта
+       1. Потребность о трудоустройстве - ДА или НЕТ
+       2. Потребность о создании AI проекте - ДА или НЕТ
+       3. Возражение о стоимости - ДА или НЕТ
+       4. Возражение о времени обучения - ДА или НЕТ
+
+       Заполни отчёт и пришли в качестве ответа, коротко и ёмко """, key='prompt', disabled = st.session_state.learn == None)
+
+  st.sidebar.text_area('Запрос пользователя', 'Пришли отчет',  key='query', disabled = st.session_state.learn == None)
+  st.sidebar.button('Отправить запрос', key='request', on_click=answer_request, disabled = st.session_state.learn == None)
+  
+
+   
+if __name__ == "__main__":
+    main()
